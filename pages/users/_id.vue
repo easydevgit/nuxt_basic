744defcf1cd56219c986458eebac8b9466704ee5
= <=>
2022-01-22 22:14:49 +0400

    validate route params

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        I am Bob
-    </div>
+  <div>
+    I am Bob
+  </div>
 </template>
+
+<script>
+export default {
+  validate({ params }) {
+    const idCheck = /^\d+$/.test(params.id);
+    if (idCheck === false) {
+      throw new Error("Incorrect user id");
+    }
+
+    return true;
+  },
+};
+</script>
